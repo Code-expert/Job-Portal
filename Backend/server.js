34c3b9a64717a7428ebb41d9aea2052b94c92e12
Git-demo <hrs899800@gmail.com>
2025-01-24 20:29:58 +0530

    Basic

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,33 @@
-const {express} = require('express');
+import  express  from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoDb from './Connection/connect.js';
 const app = express();
-const port =3000;
+dotenv.config({});
+
+
+  
+
+//Middlewares...
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+
+
+const PORT =process.env.PORT||3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  mongoDb();
+  console.log(`Server is running on port ${PORT}`);
 });

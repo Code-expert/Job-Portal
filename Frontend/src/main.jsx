dd378ad937e7a750cecf813ca836103f16961c93
Git-demo <hrs899800@gmail.com>
2025-01-31 23:17:25 +0530

    navabr

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,43 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Signup from './components/Signup.jsx'
+import Login from './components/Login.jsx'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                    <Login />               
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+               
+                    <Signup />
+                
+            ),
+        },
+      ],
+    }
+      ])
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter}/>
+   
   </StrictMode>,
 )
